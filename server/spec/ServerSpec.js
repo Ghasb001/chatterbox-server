50365d7baf80baa238375f8dc46a61e549ff6d29
Ariel <60643935+Waittill@users.noreply.github.com>
2022-11-01 17:13:28 -0700

    Complete urls for chat client

--- server/spec/ServerSpec.js
+++ server/spec/ServerSpec.js
@@ -65,6 +65,7 @@ describe('Node Server Request Listener Function', function() {
 
     handler.requestHandler(req, res);
 
+
     expect(res._responseCode).to.equal(201);
 
     // Now if we request the log for that room the message we posted should be there:
@@ -74,7 +75,9 @@ describe('Node Server Request Listener Function', function() {
     handler.requestHandler(req, res);
 
     expect(res._responseCode).to.equal(200);
+
     var messages = JSON.parse(res._data);
+
     expect(messages.length).to.be.above(0);
     expect(messages[0].username).to.equal('Jono');
     expect(messages[0].text).to.equal('Do my bidding!');
